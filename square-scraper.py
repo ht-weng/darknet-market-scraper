6cb004174c41e0dbb456018365f487f2c1d6795b
Haotian Weng <haotian.weng@hotmail.com>
2020-04-04 18:48:55 +1100

    init

---
+++ square-scraper.py
@@ -0,0 +1,39 @@
+# Square Market Scraper
+
+from bs4 import BeautifulSoup
+import re
+
+record_count = 10
+website_url = 'http://c77yo2fe3f4e3g7tll5qrzzoneymihws2tpjceg6wop2ky6pkcaqczyd.onion'
+
+with open('square2.html', 'r') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+listings = soup.select('div.is-5')
+price_list = soup.select('div.is-3')
+
+for i in range(record_count):
+    product = listings[i].select('a')[0].get_text().strip()
+    product_url = listings[i].select('a')[0].get('href')
+    price_in_btc = float(price_list[0].select('p')[0].get_text().strip().replace(' BTC', ''))
+    vendor = listings[i].select('a')[1].get_text().strip()
+    vendor_url = listings[i].select('a')[1].get('href')
+    vendor_level = listings[i].select('span')[2].get_text()
+    vendor_pos_rating = int(listings[i].select('span')[0].get_text().replace('+', ''))
+    vendor_neg_rating = int(listings[i].select('span')[1].get_text())
+    
+    shipping_text = listings[i].select('p')[1].get_text()
+    try:
+        shipping_from = re.search('Ships from (.+?)\n', shipping_text).group(1)
+    except AttributeError:
+        shipping_from = 'N/A'
+    
+    print('Product: ', product)
+    print('Product URL: ', product_url)
+    print('Price in BTC: ', price_in_btc)
+    print('Vendor: ', vendor)
+    print('Vendor URL: ', vendor_url)
+    print('Vendor Level: ',vendor_level)
+    print('Vendor Positive Rating: ', vendor_pos_rating)
+    print('Vendor Negative Rating: ', vendor_neg_rating)
+    print('Shipping From: ', shipping_from)
---
+++ yellow-scraper.py
@@ -0,0 +1,36 @@
+# Yellow Brick Market Scraper
+
+from bs4 import BeautifulSoup
+import re
+
+record_count = 11
+website_url = 'http://ck73ugjvx5a4wkhsmrfvwhlrq7evceovbsb7tvaxilpahybdokbyqcqd.onion/'
+
+with open('yellow2.html', 'r') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+listings = soup.select('div#listing')
+
+for i in range(record_count):
+    product = listings[i].select('a')[1].get_text().strip()
+    product_url = website_url + listings[i].select('a')[1].get('href')
+    price_text = listings[i].select('b')[0].get_text().replace('$', '')
+    if price_text != 'Highly Sold':
+        price_in_usd = float(price_text)
+        vendor = listings[i].select('span')[0].select('a')[0].get_text()
+        vendor_url = website_url + listings[i].select('span')[0].select('a')[0].get('href')
+    else:
+        price_in_usd = float(listings[i].select('b')[1].get_text().replace('$', ''))
+        vendor = listings[i].select('span')[1].select('a')[0].get_text()
+        vendor_url = website_url + listings[i].select('span')[1].select('a')[0].get('href')
+    
+    category = listings[i].select('a')[3].get_text()
+    shipping_from = listings[i].select('span')[2].get_text()
+    
+    print('Product: ', product)
+    print('Product URL: ', product_url)
+    print('Price in USD: ', price_in_usd)
+    print('Category: ', category)
+    print('Vendor: ', vendor)
+    print('Vendor URL: ', vendor_url)
+    print('Shipping From: ', shipping_from)
